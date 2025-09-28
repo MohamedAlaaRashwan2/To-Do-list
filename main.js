19cbf9f0ed0d78bbcfb217f875fa3d8fade374f8
Mohamed Rashwan <mohaalaa092@gmail.com>
2025-09-28 06:32:03 +0300

    add

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-
 const inputTask = document.getElementById("task");
 const submitTask = document.getElementById("add");
 const tasksContainer = document.getElementById("tasks");
@@ -16,12 +15,26 @@ if (window.localStorage.getItem("tasks")) {
 submitTask.addEventListener("click", (e) => {
     e.preventDefault();
     if (inputTask.value) {
-        const task = {
-            id: Date.now(),
-            title: inputTask.value,
-            completed: false,
+        if (editId !== 0) {
+            // تعديل المهمة
+            tasks = tasks.map((task) => {
+                if (task.id === editId) {
+                    return { ...task, 
+                        title: inputTask.value 
+                    };
+                }
+                return task;
+            });
+            editId = 0;
+        } else {
+            // إضافة مهمة جديدة
+            const task = {
+                id: Date.now(),
+                title: inputTask.value,
+                completed: false,
+            };
+            tasks.push(task);
         }
-        tasks.push(task);
         inputTask.value = "";
         renderTasks();
         saveTasks();
@@ -34,11 +47,15 @@ function renderTasks() {
     tasksContainer.innerHTML = "";
     tasks.forEach((task) => {
         const li = document.createElement("li");
-        li.className = task.title;
-        li.innerHTML = `
+        if (task.title !== "") {
+            li.className = task.title;
+            li.innerHTML = `
         <span onclick="chackTask(this, ${task.id})" class="${task.completed ? 'chack' : ''}">${task.title}</span>
-        <button class="delete" onclick="deleteTask(${task.id})">X</button>`;
-        tasksContainer.prepend(li);
+        <div>
+        <button class="edit" onclick="editTask(${task.id})"><i class="fa-solid fa-pen"></i></button>
+        <button class="delete" onclick="deleteTask(${task.id})"><i class="fa-solid fa-trash"></i></button>
+        </div>`;
+        tasksContainer.prepend(li);}
     })
 
 };
@@ -54,6 +71,7 @@ function chackTask(span, id) {
     tasks = tasks.map((task) => {
         if (task.id === id) {
             return {
+                ...task,
                 completed: span.classList.contains("chack"),
             };
         }
@@ -66,71 +84,15 @@ function saveTasks() {
     window.localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+let editId = 0;
 
+function editTask(id) {
+    const task = tasks.find((task) => task.id === id);
+    if (task) {
+        inputTask.value = task.title;
+        editId = id;
+        inputTask.focus();
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if (window.localStorage.getItem("tasks")) {
-//     tasks = JSON.parse(window.localStorage.getItem("tasks"));
-//     console.log(tasks);
-//     renderTasks();
-// }
-
-// submitTask.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     if (inputTask.value) {
-//         const task = {
-//             id: Date.now(),
-//             title: inputTask.value,
-//             completed: false
-//         }
-//         tasks.push(task);
-//         inputTask.value = "";
-//         renderTasks();
-//         saveTasks();
-//     } else {
-//         alert("Please enter a task");
-//     }
-// });
-
-
-// function saveTasks() {
-//     window.localStorage.setItem("tasks", JSON.stringify(tasks));
-// }
-
-// function renderTasks() {
-//     tasksContainer.innerHTML = "";
-//     tasks.forEach((task) => {
-//         const li = document.createElement("li");
-//         li.className = task.title;
-//         li.innerHTML = `
-//         <span>${task.title}</span>
-//         <button class="delete" onclick="deleteTask(${task.id})">X</button>`;
-//         tasksContainer.appendChild(li);
-//     });
-// }
-
-// function deleteTask(id) {
-//     tasks = tasks.filter((task) => task.id !== id);
-//     saveTasks();
-//     renderTasks();
-// }
 
